db19b6f1aa8e1f1606cf14388972cf3370e545e2
leritt1 <Lizzka.g@bk.ru>
2025-09-12 12:37:19 +0500

    init project

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const cards  = document.querySelectorAll('.card'); //выбираем все элементы с классом card на странице( без All 1 элемент)
+for (let card of cards) {
+    card.addEventListener("click", ()=>{
+    card.classList.toggle("card-show");
+});
+}
