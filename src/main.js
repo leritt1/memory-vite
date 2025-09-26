65146242768aaa07d29cc89e7d17ac8916c7da2a
leritt1 <Lizzka.g@bk.ru>
2025-09-26 12:51:18 +0500

    working with sass

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+
 const cards  = document.querySelectorAll('.card'); //выбираем все элементы с классом card на странице( без All 1 элемент)
 for (let card of cards) {
     card.addEventListener("click", ()=>{
